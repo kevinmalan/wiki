cd126fa2e45839b405040be526571ff0334ef947
Kevin Malan <kmalan@outlook.com>
2020-11-07 16:18:35 +0200

    Projects Setup

---
+++ DomainWiki.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DomainWiki.API.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"DomainWiki.API is running...");
+        }
+    }
+}
