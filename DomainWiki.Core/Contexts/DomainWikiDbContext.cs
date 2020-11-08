41e58856de086979a69ecf1074aa3a7845d17248
Kevin Malan <kmalan@outlook.com>
2020-11-08 18:01:16 +0200

    Add EF DbContext

---
+++ DomainWiki.Core/Contexts/DomainWikiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DomainWiki.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainWiki.Core.Contexts
+{
+    public class DomainWikiDbContext : DbContext
+    {
+        public DomainWikiDbContext(DbContextOptions<DomainWikiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain> Domain { get; set; }
+        public DbSet<Group> Group { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Wiki> Wiki { get; set; }
+        public DbSet<Folder> Folder { get; set; }
+        public DbSet<Document> Document { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+        public DbSet<DocumentTagConnection> DocumentTagConnection { get; set; }
+    }
+}
