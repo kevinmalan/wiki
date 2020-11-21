c6b62277b000d35f7a7f2f1186027a4d78379d7c
Kevin Malan <kmalan@outlook.com>
2020-11-21 17:02:05 +0200

    Add Logging and SEQ Sink

--- DomainWiki.API/Controllers/HealthController.cs
+++ DomainWiki.API/Controllers/HealthController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DomainWiki.API.Controllers
 {
     public class HealthController : Controller
     {
+        private readonly ILogger logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            this.logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
+            logger.LogInformation($"DomainWiki.API is running...");
             return Ok($"DomainWiki.API is running...");
         }
     }
--- DomainWiki.API/Program.cs
+++ DomainWiki.API/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,6 +13,11 @@ namespace DomainWiki.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+               .ConfigureLogging(l =>
+               {
+                   l.ClearProviders();
+                   l.AddSeq();
+               })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
