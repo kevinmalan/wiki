1e5e54438a69d8505b9995437765ae500c38f375
kevinmalan <kmalan@outlook.com>
2021-04-27 13:37:36 +0200

    Added Env setup script

--- Scripts/2.Publish-dacpac.ps1
+++ Scripts/2.Publish-dacpac.ps1
@@ -1,7 +1,15 @@
-$SourceFile = "../Wiki.SQL/bin/Debug/Wiki.SQL.dacpac"
+$SourceFile = "./Wiki.SQL/bin/Debug/Wiki.SQL.dacpac"
 $Database = "wikiDb"
 $SourceServer = "localhost"
 $UserId = "sa"
 $Password = 'DevDbP@ssword'
+
+# Add Env var if not exists
+$path = "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin"
+if (!(test-path $path))
+{
+    New-Item -ItemType Directory -Force -Path $path
+}
+
 
 sqlpackage /action:Publish /SourceFile:$SourceFile /TargetConnectionString:"data source=$SourceServer;initial catalog=$Database;User ID=$UserId;Password=$Password"
---
+++ SetupEnv.ps1
@@ -0,0 +1,2 @@
+./Scripts/1.Run-db-container.ps1
+./Scripts/2.Publish-dacpac.ps1
